db8fb6de7f6d71afeea9129ad95665657d3adb65
Alfredo Aguirre <alfredo@madewithbyt.es>
2016-11-20 22:20:56 +0000

    Register `X-Frame-Options` header.

--- src/headers/mod.rs
+++ src/headers/mod.rs
@@ -1,3 +1,5 @@
 header! { (ContentSecurityPolicy, "Content-Security-Policy") => [String] }
 
 header! { (ContentSecurityPolicyReportOnly, "Content-Security-Policy-Report-Only") => [String] }
+
+header! { (XFrameOptions, "X-Frame-Options") => [String] }
