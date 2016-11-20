a2e99725646980c6ab3b086223fbeb5042fcd0a9
Alfredo Aguirre <alfredo@madewithbyt.es>
2016-11-20 14:08:33 +0000

    Add APPROVED and FAILED constants.
    Used to reuse them across modules.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #[macro_use] extern crate emojicons;
 extern crate hyper;
 
-
 use hyper::client::Client;
+
 pub mod checklist;
+
+
+static APPROVED: &'static str = ":white_check_mark:";
+static FAILED: &'static str = ":no_entry:";
 
 
 fn main() {
