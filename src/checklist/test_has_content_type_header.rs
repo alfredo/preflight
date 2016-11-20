d4af24d6f5ed54a4b8ebfc039cc09d3d7bf22689
Alfredo Aguirre <alfredo@madewithbyt.es>
2016-11-20 21:31:53 +0000

    Use `header` object to read the response.

--- src/checklist/test_has_content_type_header.rs
+++ src/checklist/test_has_content_type_header.rs
@@ -1,10 +1,11 @@
+use hyper::header;
 use hyper::client::response::Response;
 
 
 pub fn execute(response: &Response) -> bool {
-    let content_type_header = response.headers.get_raw("Content-Type");
+    let content_type_header = response.headers.get::<header::ContentType>();
     if content_type_header.is_some() {
-        ::notifications::approved("Site defines `Content-Type`");
+        ::notifications::approved(&format!("Site defines `Content-Type: {}`", content_type_header.unwrap()));
     } else {
         ::notifications::failed("Site does not define `Content-Type`");
     }
